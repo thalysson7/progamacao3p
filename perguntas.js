c673e754a6bca0cd1fdc352e24513859af0da284
thalysson7 <137517077+thalysson7@users.noreply.github.com>
2024-10-17 09:46:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'sobrevivencia',
+    'qual o eletro domestico preferido do batman?',
+    'bat-deira'
+)
+
+criaCartao(
+    'Geografia',
+    'o que a mosca foi fazer na russia?',
+    'passear em moscou'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
